16f6919b25e47e7a5a3f44cfc85992dc47f585e6
tarahform <ptarahform@gmail.com>
2018-05-03 13:54:25 -0500

    javascript pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// connect to API's //
+
+// end of API //
+// -------------//
+
+
+// connect to FireBase
+
+// end of FireBase //
+// -------------//
+
+
+// Global Variables //
+
+//End of GLobal Variables //
+// -------------//
+
+
+// Functionality //
+
+// End of Functionality //
+// -------------//
